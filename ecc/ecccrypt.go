24f49c7ca7c3e2b85ac6db887994546ebc9096eb
SongZihuan <songzihuan@song-zh.com>
2023-04-05 18:29:54 +0800

    Initial commit

---
+++ ecc/ecccrypt.go
@@ -0,0 +1,113 @@
+package ecc
+
+import (
+	"crypto/ecdsa"
+	"crypto/rand"
+	"crypto/x509"
+	"encoding/base64"
+	"encoding/hex"
+	"fmt"
+	"os"
+	"runtime"
+)
+
+// The public key and plaintext are passed in for encryption
+func eccEncrypt(plainText, pubKey []byte) (cipherText []byte, err error) {
+	defer func() {
+		if err := recover(); err != nil {
+			switch err.(type) {
+			case runtime.Error:
+				// TODO 加入到日志中
+				_, _ = fmt.Fprintf(os.Stderr, "runtime err=%v,Check that the key or text is correct", err)
+			default:
+				_, _ = fmt.Fprintf(os.Stderr, "error=%v,check the cipherText ", err)
+			}
+		}
+	}()
+	tempPublicKey, err := x509.ParsePKIXPublicKey(pubKey)
+	if err != nil {
+		return nil, err
+	}
+	// Decode to get the private key in the ecdsa package
+	publicKey1 := tempPublicKey.(*ecdsa.PublicKey)
+	// Convert to the public key in the ecies package in the ethereum package
+	publicKey := ImportECDSAPublic(publicKey1)
+	cipherText, err = Encrypt(rand.Reader, publicKey, plainText, nil, nil)
+	return cipherText, err
+
+}
+
+// The private key and plaintext are passed in for decryption
+func eccDecrypt(cipherText, priKey []byte) (msg []byte, err error) {
+	defer func() {
+		if err := recover(); err != nil {
+			switch err.(type) {
+			case runtime.Error:
+				// TODO 加入到日志中
+				_, _ = fmt.Fprintf(os.Stderr, "runtime err=%v,Check that the key or text is correct", err)
+			default:
+				_, _ = fmt.Fprintf(os.Stderr, "error=%v,check the cipherText ", err)
+			}
+		}
+	}()
+	tempPrivateKey, err := x509.ParseECPrivateKey(priKey)
+	if err != nil {
+		return nil, err
+	}
+	// Decode to get the private key in the ecdsa package
+	// Convert to the private key in the ecies package in the ethereum package
+	privateKey := ImportECDSA(tempPrivateKey)
+	plainText, err := privateKey.Decrypt(cipherText, nil, nil)
+	if err != nil {
+		return nil, err
+	}
+	return plainText, nil
+}
+
+func EccEncryptToBase64(plainText []byte, base64PubKey string) (base64CipherText string, err error) {
+	pub, err := base64.StdEncoding.DecodeString(base64PubKey)
+	if err != nil {
+		return "", err
+	}
+	cipherBytes, err := eccEncrypt(plainText, pub)
+	if err != nil {
+		return "", err
+	}
+	return base64.StdEncoding.EncodeToString(cipherBytes), nil
+}
+
+func EccDecryptByBase64(base64CipherText, base64PriKey string) (plainText []byte, err error) {
+	privateBytes, err := base64.StdEncoding.DecodeString(base64PriKey)
+	if err != nil {
+		return nil, err
+	}
+	cipherTextBytes, err := base64.StdEncoding.DecodeString(base64CipherText)
+	if err != nil {
+		return nil, err
+	}
+	return eccDecrypt(cipherTextBytes, privateBytes)
+}
+
+func EccEncryptToHex(plainText []byte, hexPubKey string) (hexCipherText string, err error) {
+	pub, err := hex.DecodeString(hexPubKey)
+	if err != nil {
+		return "", err
+	}
+	cipherBytes, err := eccEncrypt(plainText, pub)
+	if err != nil {
+		return "", err
+	}
+	return hex.EncodeToString(cipherBytes), nil
+}
+
+func EccDecryptByHex(hexCipherText, hexPriKey string) (plainText []byte, err error) {
+	privateBytes, err := hex.DecodeString(hexPriKey)
+	if err != nil {
+		return nil, err
+	}
+	cipherTextBytes, err := hex.DecodeString(hexCipherText)
+	if err != nil {
+		return nil, err
+	}
+	return eccDecrypt(cipherTextBytes, privateBytes)
+}
---
+++ main.go
@@ -0,0 +1,110 @@
+package main
+
+import (
+	"fmt"
+	"git.wuntsong.com/wunchain/goecc/ecc"
+)
+
+func main() {
+	fmt.Printf("CryptBase64:\n")
+	cryptBase64()
+
+	fmt.Printf("CryptHex:\n")
+	cryptHex()
+
+	fmt.Printf("SignBase64:\n")
+	signBase64()
+
+	fmt.Printf("SignHex:\n")
+	signHex()
+}
+
+func cryptBase64() {
+	msg := "ABCDEFG"
+	eccBase64Key, err := ecc.GenerateEccKeyBase64()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Printf("Pri: %s\n", eccBase64Key.PrivateKey)
+	fmt.Printf("Pub: %s\n", eccBase64Key.PublicKey)
+
+	base64Text, err := ecc.EccEncryptToBase64([]byte(msg), eccBase64Key.PublicKey)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Printf("Secret:%s\n", base64Text)
+
+	plaintext, err := ecc.EccDecryptByBase64(base64Text, eccBase64Key.PrivateKey)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Printf("Plain:%s\n", string(plaintext))
+}
+
+func cryptHex() {
+	msg := "ABCDEFG"
+	eccBase64Key, err := ecc.GenerateEccKeyHex()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Printf("Pri: %s\n", eccBase64Key.PrivateKey)
+	fmt.Printf("Pub: %s\n", eccBase64Key.PublicKey)
+
+	base64Text, err := ecc.EccEncryptToHex([]byte(msg), eccBase64Key.PublicKey)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Printf("Secret:%s\n", base64Text)
+
+	plaintext, err := ecc.EccDecryptByHex(base64Text, eccBase64Key.PrivateKey)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Printf("Plain:%s\n", string(plaintext))
+}
+
+func signBase64() {
+	msg := "ABCDEFG"
+	eccBase64Key, err := ecc.GenerateEccKeyBase64()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Printf("Pri: %s\n", eccBase64Key.PrivateKey)
+	fmt.Printf("Pub: %s\n", eccBase64Key.PublicKey)
+
+	rSign, sSign, err := ecc.EccSignBase64([]byte(msg), eccBase64Key.PrivateKey)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Printf("R_Sign: %s\nS_Sign: %s\n", rSign, sSign)
+	res := ecc.EccVerifySignBase64([]byte(msg), rSign, sSign, eccBase64Key.PublicKey)
+	fmt.Printf("Sign:%v\n", res)
+}
+
+func signHex() {
+	msg := "ABCDEFG"
+	eccBase64Key, err := ecc.GenerateEccKeyHex()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Printf("Pri: %s\n", eccBase64Key.PrivateKey)
+	fmt.Printf("Pub: %s\n", eccBase64Key.PublicKey)
+
+	rSign, sSign, err := ecc.EccSignHex([]byte(msg), eccBase64Key.PrivateKey)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Printf("R_Sign: %s\nS_Sign: %s\n", rSign, sSign)
+	res := ecc.EccVerifySignHex([]byte(msg), rSign, sSign, eccBase64Key.PublicKey)
+	fmt.Printf("Sign: %v\n", res)
+}
